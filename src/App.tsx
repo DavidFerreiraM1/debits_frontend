f161fccc33c23532f007557cfc2bfe20cbb1b2ea
David <davidferreira.m1@outlook.com>
2021-03-11 07:38:27 -0300

    implementação incial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, Grid } from '@material-ui/core';
+import { DebitList } from './features/debit-list';
+import { UsersList } from './features/user-list';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <Container maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item xs={4}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Box component="section">
+                  <DebitList />
+                </Box>
+              </Grid>
+              <Grid item xs={12}>
+                <Box component="section">
+                  <UsersList />
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={8}>
+            <div style={{ height: 320, width: '100%', background: 'blue' }} />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
